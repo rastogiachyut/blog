29dd2e2ad4559eda26569741e1d20b544f2456fe
Achyut Rastogi <rastogiachyut@gmail.com>
2016-11-30 00:47:03 +0530

    Change for diffrent cinfig

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -1,11 +1,5 @@
 class Avatar < ApplicationRecord
-  has_attached_file :image, 
-                  styles: { medium: "300x300>", thumb: "100x100>" }
-                  storage: :s3,
-                  bucket: 'achyut-blog-assets',
-                  s3_credentials: "#{Rails.root}/config/aws.yml",
-                  path: "resources/:id/:style/:basename.:extension"
-                  #, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }#, default_url: "/images/:style/missing.png"
   validates_attachment_file_name :image, matches: [/png\Z/, /jpe?g\Z/, /gif\Z/]
   validates_attachment :image, presence: true, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }#, size: { in: 0..10.kilobytes }
 
