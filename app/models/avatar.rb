2ef18476b51e3cbadd2f7685a2aac0ab4cd7cd95
Achyut Rastogi <rastogiachyut@gmail.com>
2016-12-06 20:04:13 +0530

    Fix image source

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -5,4 +5,10 @@ class Avatar < ApplicationRecord
 
   belongs_to :user
 
+  def source(type)
+    # debugger
+    source_string = self.image.url(type)
+    source_string.slice!('/achyut-blog-assets')
+    source_string
+  end
 end
